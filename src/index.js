0a0d8b23d321cdddd518d53af689801ff01ce76d
zakariabaqasse <zbaqasse51@gmail.com>
2023-12-11 21:53:04 +0100

    update: refactored code

--- src/index.js
+++ src/index.js
@@ -16,10 +16,11 @@ app.use(Express.json());
 
 app.use("/auth", authRouter);
 // Apply authentication middleware to all routes under "/user"
-app.use("/user", expressjwt({ secret: process.env.SECRET || 'Bearer', algorithms: ["HS256"] }));
-app.use("/user", userRouter);
-
-
+app.use(
+  "/user",
+  expressjwt({ secret: process.env.SECRET || "Bearer", algorithms: ["HS256"] }),
+  userRouter
+);
 
 app.listen(8888, async () => {
   await connectToDB();
